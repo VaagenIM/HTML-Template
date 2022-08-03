e028590c6379c4e3ee93ebb520eecb6df705e7c5
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-08-03 23:17:09 +0200

    Update test_links.py

--- .github/workflows/test_links.py
+++ .github/workflows/test_links.py
@@ -3,18 +3,18 @@ import glob
 
 from pathlib import Path
 
-
 ROOT_FOLDER = str(Path(__file__).parents[2]).replace('\\', '/')
 FILES = [file.replace('\\', '/') for file in glob.glob(f'{ROOT_FOLDER}/**/*.*', recursive=True)]
 CONTENTS = [open(file, 'r').read() for file in FILES if file.lower().endswith('.html')]
 
 
-def verify_attr_value(value: str) -> bool:
-    if re.match(r'^https?://', value, flags=re.IGNORECASE):
+def validate_relative_path(rel_path: str) -> bool:
+    """Raises FileNotFoundError if the given path is not valid or an URL"""
+    is_link = re.match(r'^https?://', rel_path, flags=re.IGNORECASE)
+    abspath = f'{ROOT_FOLDER}/{rel_path}'
+    if is_link or abspath in FILES:
         return True
-    elif f'{ROOT_FOLDER}/{value}' not in FILES:
-        raise FileNotFoundError(f'Invalid link: {value}')
-    return True
+    raise FileNotFoundError(f'Could not find the linked resource: {rel_path}')
 
 
 def regex(attr: str) -> str:
@@ -22,13 +22,13 @@ def regex(attr: str) -> str:
     return r'\<[^\>]*' + attr + r'="([^\"]*)"[^\>]*>'
 
 
-def test_href(pytestconfig):
+def test_href():
     for content in CONTENTS:
         paths = re.findall(regex('href'), content, flags=re.IGNORECASE)
-        [verify_attr_value(path) for path in paths]
+        [validate_relative_path(path) for path in paths]
 
 
 def test_src():
     for content in CONTENTS:
         paths = re.findall(regex('src'), content, flags=re.IGNORECASE)
-        [verify_attr_value(path) for path in paths]
+        [validate_relative_path(path) for path in paths]
