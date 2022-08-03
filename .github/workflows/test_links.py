2b7521f1cbc33de93cac811e47e07ab2a7ecd716
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-08-03 23:02:44 +0200

    Update test_links.py

--- .github/workflows/test_links.py
+++ .github/workflows/test_links.py
@@ -1,25 +1,23 @@
-import glob
 import re
+import glob
 
 from pathlib import Path
 
-ROOT_FOLDER = Path(__file__).parents[2]
-FILES = glob.glob(f'{ROOT_FOLDER}\\**\\*.*', recursive=True)
-HTML_FILES = glob.glob(f'{ROOT_FOLDER}\\**\\*.html', recursive=True)
-CONTENTS = [open(file, 'r').read() for file in HTML_FILES]
+
+ROOT_FOLDER = str(Path(__file__).parents[2]).replace('\\', '/')
+FILES = [file.replace('\\', '/') for file in glob.glob(f'{ROOT_FOLDER}/**/*.*', recursive=True)]
+CONTENTS = [open(file, 'r').read() for file in FILES if file.lower().endswith('.html')]
 
 
 def verify_attr_value(value: str) -> bool:
     if re.match(r'^https?://', value, flags=re.IGNORECASE):
         return True
-
-    value = value.replace('/', '\\')
-    if f'{ROOT_FOLDER}\\{value}' not in FILES:
+    elif f'{ROOT_FOLDER}/{value}' not in FILES:
         raise FileNotFoundError(f'Invalid link: {value}')
     return True
 
 
-def regex(attr):
+def regex(attr: str) -> str:
     """Returns <*{attr}="(*CAPTURE*)"*>"""
     return r'\<[^\>]*' + attr + r'="([^\"]*)"[^\>]*>'
 
@@ -31,9 +29,6 @@ def test_href(pytestconfig):
 
 
 def test_src():
-    print(ROOT_FOLDER)
-    print(HTML_FILES)
-    assert False
     for content in CONTENTS:
         paths = re.findall(regex('src'), content, flags=re.IGNORECASE)
         [verify_attr_value(path) for path in paths]
